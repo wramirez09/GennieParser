d7e002f915c07b0cf4afa1cb2d19ecb1054351d5
Will Ramirez <wramirez1980@gmail.com>
2022-05-01 21:24:49 -0500

    add cors package

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
 const DataApp = require("./index");
 const dataApp = DataApp();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get("/", async (req, res) => {
   let data = await dataApp.loadData();
