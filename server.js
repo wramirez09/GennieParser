ca280d486d5a5f6cc71e30bcea8f012c4f1add4a
Will Ramirez <wramirez1980@gmail.com>
2022-05-01 21:01:39 -0500

    update to allow cors

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const port = 3000;
 const DataApp = require("./index");
 const dataApp = DataApp();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", async (req, res) => {
   let data = await dataApp.loadData();
   if (data) res.json(data);
